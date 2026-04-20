b71a9fc09dc8e845e2d6f1b004f151462cf4bec6
Fermin Blanco Ramirez <blancoramirezfermin@gmail.com>
2026-04-20 10:07:49 +0200

    PORFIN ME CAGO EN DIOS

--- src/cole/testeoCole.java
+++ src/cole/testeoCole.java
@@ -1,9 +1,10 @@
-
+package cole;
 public class testeoCole{
     static void main(String[] args) {
         Estudiante estudiante = new Estudiante("Juan", "Avenida de madrid 12", "1ºIngenieria de Software",
                 2);
-        Profesor profesor = new Profesor("Rodrigo", "Calle coruña 12", "Ingenieria", "Informatica")
+        Profesor profesor = new Profesor("Rodrigo", "Calle coruña 12", "Ingenieria", "Informatica");
         System.out.println(estudiante);
         System.out.println(profesor);
+    }
 }
